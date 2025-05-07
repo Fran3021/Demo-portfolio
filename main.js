e1ab70fd0d6b51af1d2362eb3770d8ecf74c89d1
Fran <frangarcia3021@gmail.com>
2025-05-07 13:02:40 +0200

    actualizamos con forward y use

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import './sass/style.scss'
 
+let formulario = document.querySelector('.formulario')
+let inputEmail = document.querySelector('.email')
+let iconError = document.querySelector('.icon-error-email')
+let mensajeError = document.querySelector('.mensaje-error-email')
 
+formulario.addEventListener('submit', (event) => {
+    if(!inputEmail.validity.valid){
+        event.preventDefault()
+        inputEmail.classList.add('invalid')
+        iconError.style.display = 'block'
+        mensajeError.style.display = 'block'
+    }
+})
+
+inputEmail.addEventListener('input', () => {
+    if(inputEmail.validity.valid){
+        inputEmail.classList.remove('invalid')
+        iconError.style.display = 'none'
+        mensajeError.style.display = 'none'
+    }
+})
